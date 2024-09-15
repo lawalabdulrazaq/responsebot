dae4e1255991c5b873a3bcf46f64826abd06ed1f
FABIAN-THEWISE <orkarmelch@gmail.com>
2024-09-15 12:19:37 +0100

    created a response bot

---
+++ responsebot.py
@@ -0,0 +1,22 @@
+import random
+import asyncio,telebot
+from telebot.async_telebot import AsyncTeleBot
+from telebot.handler_backends import StatesGroup, State
+
+bot_token = "7509694550:AAHaZ7nEckGYEatAyUPXvdpaK44aS8LRFmU"
+bot = AsyncTeleBot(bot_token)
+
+
+@bot.message_handler(content_types=['text'])
+async def respond_to_me(message):
+    try:
+        # extract message sent by user
+        user_message = message.text
+        message_to_send = f"this is your message:\n{user_message}"
+        # respond to user
+        await bot.reply_to(message,message_to_send)
+    except Exception as me:
+        print("respond_to_me:", me)
+
+asyncio.run(bot.polling())        
+        
